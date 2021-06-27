8a1240c30adf5bb50dbe81c7ccb792fddbbd26ca
odx <65030598+odxscholars@users.noreply.github.com>
2021-06-27 19:11:31 +0800

    Add files via upload

--- ColemanLiauGUI_Main.py
+++ ColemanLiauGUI_Main.py
@@ -4,61 +4,100 @@ import tkinter as tk
 import sys
 import os
 
-def main_test(param_):
-    test = open(param_)
-    t_var = test.read()
-    text_edited = t_var.replace("!", ".").replace("?",".").replace("\n", "")
-    letterc = 0
-    spacec = 0
-    puncc = 0
+def CL_Index(txt):
+    
+    # opens text file 
+    open_file = open(txt)
+    
+    # reads text file
+    read_file = open_file.read()
+    
+    # replaces sentence-ending punctuations with periods to indicate  the end of a sentence
+    text_edited = read_file.replace("!", ".").replace("?",".").replace("\n", "")
+    
+    # variable declarations 
+    letter_counter = 0
+    space_counter = 0
+    punctuation_counter = 0
+    
+    # returns length of string used in the loops 
     string_length = len(text_edited)
     
+    # counts the number of letters and spaces 
     for i in range(0, string_length, 1):
         if text_edited[i].isalpha() == True:
-            letterc += 1
+            letter_counter += 1
         elif text_edited[i].isspace() == True:
-           spacec += 1
+           space_counter += 1
     
-    final_resultL = (letterc / (spacec - 1) * 100)
-
+    # counts the number of sentences based on periods
     string_length = len(text_edited)
     for i in range(0, string_length, 1):
         if text_edited[i] == ".":
-            puncc += 1
+            punctuation_counter += 1
     
-    final_resultS = (puncc / (spacec - 1) * 100)
     # CLI = 0.0588L - 0.296S - 15.8
-    # L = l / w * 100
-    # S = s / w *100
-    word_count = final_resultL
-    sentence_count = final_resultS
-    print("letterc is ", letterc)
-    print("# of sentence-ending punctuations", puncc,"\n" "# of spaces: ", spacec)
-    final_resultF = (((0.0588 * ((letterc / (spacec + 1.0)) * 100.0)) - (0.296 * ((puncc / (spacec + 1.0)) * 100.0))) - 15.8)
-    print("Approximate value of the CLI: ", final_resultF)
-    return final_resultF
+    # L = letters / words * 100 (average number of letters per 100 words)
+    # S = sentences / words * 100 (average number of sentences per 100 words)
 
+    # prints out the number of letters
+    print("# of letters: ", letter_counter)
+    
+    # prints out the number of sentence-ending punctuations 
+    print("# of sentence-ending punctuations", punctuation_counter)
+    
+    # prints out the number of spaces 
+    print("# of spaces: ", space_counter)
+    
+    # calculates coleman-liau index (CLI)
+    CLI = (((0.0588 * ((letter_counter / (space_counter + 1.0)) * 100.0)) - (0.296 * ((punctuation_counter / (space_counter + 1.0)) * 100.0))) - 15.8)
+    
+    # prints CLI 
+    print("Approximate value of the CLI: ", CLI)
+    
+    return CLI
+
+# GUI 
 root= Tk()
 root.title('Our DefHacks 3.0 submission')
-col_ = '#FF947F'
-# bg = col changes the color of background
-canvas1 = Canvas(root, width = 800, height = 300, bg = col_, highlightthickness= 0)
-text1 = Label(text="Text file/s should be in the same directory of the program", font = 'Consolas 14', bg = col_)
+
+# changes the color of background
+background_color = '#FF947F'
+
+# defines what the GUI looks like 
+GUI_Canvas = Canvas(root, width = 800, height = 300, bg = background_color, highlightthickness= 0)
+
+# tells the user where to input files 
+label_text = Label(text="Text file/s should be in the same directory of the program", font = 'Consolas 14', bg = background_color)
+
+# colonel sanders logo 
 my_img = ImageTk.PhotoImage(Image.open("150.png"))
 my_label = Label(image=my_img)
+
+# size of logo 
 my_label.pack(pady=20)
-text1.pack(pady=30)
-canvas1.pack()
-root['bg'] = col_
-entry1 = Entry()
-canvas1.create_window(400, 10, window=entry1)
- 
+
+# size of label 
+label_text.pack(pady=30)
+
+# organizes widgets in blocks 
+GUI_Canvas.pack()
+
+
+root['bg'] = background_color
+
+# string input window 
+text_entry = Entry()
+GUI_Canvas.create_window(400, 10, window=text_entry)
+
+# calls function 
 def driverFunc(): 
-    x1 = entry1.get()
-    print_this = str(round(main_test(x1)))
-    label1 = Label(root, text= ("This given text is for Grade " + print_this + " students\nRun the program again to check for more text files!"), font= 'Consolas', bg=col_)
-    canvas1.create_window(400, 200, window=label1)
+    x1 = text_entry.get()
+    print_this = str(round(CL_Index(x1)))
+    output_text = Label(root, text= ("This given text is for Grade " + print_this + " students\nRun the program again to check for more text files!"), font= 'Consolas', bg=background_color)
+    GUI_Canvas.create_window(400, 200, window=output_text)
  
+# button to run program 
 button1 = Button(text='Get CLI Index', command=driverFunc, bg = '#C1264E', font='Consolas 15')
-canvas1.create_window(400, 50, window=button1)
+GUI_Canvas.create_window(400, 50, window=button1)
 root.mainloop()
